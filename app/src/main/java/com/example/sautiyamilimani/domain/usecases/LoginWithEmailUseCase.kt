e38b8d4f94fed29a41196513c09dbeebfc6c4099
Mwavuna <gregsonmwavuna3@gmail.com>
2025-09-27 15:59:08 +0300

    initial comit

---
+++ app/src/main/java/com/example/sautiyamilimani/domain/usecases/LoginWithEmailUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.sautiyamilimani.domain.usecases
+
+import com.example.sautiyamilimani.domain.model.Resource
+import com.example.sautiyamilimani.domain.model.User
+import com.example.sautiyamilimani.domain.repository.AuthRepository
+import javax.inject.Inject
+
+//@Inject constructor tells dagger hilt to inject an implementation of a certain repository it provides that has this return type
+class LoginWithEmailUseCase @Inject constructor(
+    private val repository: AuthRepository
+){
+    suspend operator fun invoke( email:String, password:String): Resource<User> {
+        return repository.loginWithEmail( email,password)
+    }
+}
